3be31e98c8cb523daed1840e3c6251ee902fd184
Tamra Myers <tamram@microsoft.com>
2016-09-06 15:44:40 -0700

    updates to samples to align with ref docs

---
+++ BlobStorage/WCFBufferManagerAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System.ServiceModel.Channels;
+using Microsoft.WindowsAzure.Storage;
+
+namespace BlobStorage
+{
+    public class WCFBufferManagerAdapter : IBufferManager
+    {
+        private int defaultBufferSize = 0;
+
+        public WCFBufferManagerAdapter(BufferManager manager, int defaultBufferSize)
+        {
+            this.Manager = manager;
+            this.defaultBufferSize = defaultBufferSize;
+        }
+
+        public BufferManager Manager { get; internal set; }
+
+        public void ReturnBuffer(byte[] buffer)
+        {
+            this.Manager.ReturnBuffer(buffer);
+        }
+
+        public byte[] TakeBuffer(int bufferSize)
+        {
+            return this.Manager.TakeBuffer(bufferSize);
+        }
+
+        public int GetDefaultBufferSize()
+        {
+            return this.defaultBufferSize;
+        }
+    }
+}
